f692cbdf76a042a6a5a12c9deb0e44ab2b334491
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-12-04 14:06:18 -0400

    feat: add a terminal theme

--- ubuntu/ubuntu20.04-wsl.sh
+++ ubuntu/ubuntu20.04-wsl.sh
@@ -16,3 +16,8 @@ sudo apt install ${pacotes[@]} -y
 
 sudo usermod -aG docker $USER
 sudo chsh -s $(which zsh)
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+cp "$(pwd)/${0%/*}/.p10k.zsh" ~/.p10k.zsh
+echo 'source ~/.p10k.zsh' >>~/.zshrc
