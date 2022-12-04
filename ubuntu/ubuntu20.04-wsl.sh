fc0d4cd155be8eb9d47a92ec1e3b71d6ba10f805
Rodrigo Salvio <rodrigosalvio@hotmail.com>
2022-12-04 14:05:40 -0400

    feat: add essentials packages and libs

--- ubuntu/ubuntu20.04-wsl.sh
+++ ubuntu/ubuntu20.04-wsl.sh
@@ -7,3 +7,12 @@ sudo echo -e "systemd=true" >> /etc/wsl.conuf
 fi
 
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+
+pacotes=(curl git zsh docker.io build-essential autoconf re2c bison software-properties-common dirmngr gpg gawk)
+libs=(containerd libsqlite3-dev libpq-dev libonig-dev libfcgi-dev libfcgi0ldbl libjpeg-dev libpng-dev libssl-dev libxml2-dev libcurl4-openssl-dev libxpm-dev libgd-dev libmysqlclient-dev libfreetype6-dev libxslt1-dev libpspell-dev libzip-dev libgccjit-10-dev libreadline-dev)
+
+sudo apt install ${libs[@]} -y
+sudo apt install ${pacotes[@]} -y
+
+sudo usermod -aG docker $USER
+sudo chsh -s $(which zsh)
